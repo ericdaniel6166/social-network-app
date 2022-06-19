3630bfa0dbdc190c1df37a640b981507d1b0a9e1
eric6166 <ericnguyen6166@gmail.com>
2022-06-19 19:01:02 +0700

    add User entity

---
+++ src/main/java/com/example/socialnetworkapp/model/User.java
@@ -0,0 +1,35 @@
+package com.example.socialnetworkapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode(callSuper = false)
+public class User extends Auditable<String> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private boolean isActive;
+}
