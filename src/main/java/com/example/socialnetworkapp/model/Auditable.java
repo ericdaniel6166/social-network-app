96fa7a2694f80e142197a24233a14ecd05ad269b
eric6166 <ericnguyen6166@gmail.com>
2022-06-19 18:55:50 +0700

    add Auditable

---
+++ src/main/java/com/example/socialnetworkapp/model/Auditable.java
@@ -0,0 +1,37 @@
+package com.example.socialnetworkapp.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import java.io.Serializable;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class Auditable<U> implements Serializable {
+    @CreatedBy
+    protected U createdBy;
+
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    protected Date createdDate;
+
+    @LastModifiedBy
+    protected U lastModifiedBy;
+
+    @LastModifiedDate
+    @Temporal(TIMESTAMP)
+    protected Date lastModifiedDate;
+
+
+}
