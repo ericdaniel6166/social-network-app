6903139f948b2bf86d52c6dda167cbd5ec349f41
eric6166 <ericnguyen6166@gmail.com>
2022-06-19 18:41:00 +0700

    setup

---
+++ src/test/java/com/example/socialnetworkapp/SocialNetworkAppTests.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
