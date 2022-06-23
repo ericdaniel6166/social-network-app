41c6bf704277d04d54308d5fdd8debfcdc353f63
eric6166 <ericnguyen6166@gmail.com>
2022-06-23 21:47:27 +0700

    remove redundant import

--- src/main/java/com/example/socialnetworkapp/dto/ErrorResponseDTO.java
+++ src/main/java/com/example/socialnetworkapp/dto/ErrorResponseDTO.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
