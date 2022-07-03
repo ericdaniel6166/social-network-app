4b42749a9fd77993af8a4fdd4e7ac38e26a56d52
eric6166 <ericnguyen6166@gmail.com>
2022-07-03 11:28:00 +0700

    remove redundant comment

--- src/main/java/com/example/socialnetworkapp/controller/ForumApi.java
+++ src/main/java/com/example/socialnetworkapp/controller/ForumApi.java
@@ -34,16 +34,6 @@ public interface ForumApi {
                               @RequestParam(name = "size", required = false, defaultValue = "1") Integer size,
                               @RequestParam(name = "direction", required = false, defaultValue = "ASC") String direction,
                               @RequestParam(name = "properties", required = false, defaultValue = "id") String[] properties) throws SocialNetworkAppException;
-//
-//    @ApiOperation(value = "Sign in")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "Sign in successfully"),
-//            @ApiResponse(code = 400, message = "[Business Exception] - Bad request"),
-//            @ApiResponse(code = 404, message = "[Business Exception] - Not Found"),
-//            @ApiResponse(code = 422, message = "[Business Exception] - Unprocessable Entity"),
-//            @ApiResponse(code = 500, message = "[System Exception] - Internal server error")
-//    })
-//    ResponseEntity<?> signIn(SignInRequestDTO signInRequestDTO) throws SocialNetworkAppException;
 
 
 }
