63cbae43aa0834c7f925380070f05ef6fb6a2620
eric6166 <ericnguyen6166@gmail.com>
2022-07-03 11:24:14 +0700

    add constant for PageRequest

--- src/main/java/com/example/socialnetworkapp/utils/CommonUtils.java
+++ src/main/java/com/example/socialnetworkapp/utils/CommonUtils.java
@@ -13,6 +13,16 @@ public final class CommonUtils {
 
     public static final String CHARACTER_ASTERISK = "*";
 
+    public static final String PAGE_REQUEST_DIRECTION_ASC = "ASC";
+
+    public static final String PAGE_REQUEST_DIRECTION_DESC = "DESC";
+
+    public static final String PAGE_REQUEST_SIZE_DEFAULT = "10";
+
+    public static final String PAGE_REQUEST_PAGE_NUMBER_DEFAULT = "0";
+
+    public static final String PAGE_REQUEST_PROPERTIES_LAST_MODIFIED_DATE = "lastModifiedDate";
+
     public static String maskEmail(String email) {
         return email.replaceAll(REGEX_MASKING_EMAIL, CHARACTER_ASTERISK);
     }
