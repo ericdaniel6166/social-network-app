1c6da57e59ca7f2a3f27224a79d7bb1fac1edf06
eric6166 <ericnguyen6166@gmail.com>
2022-07-28 00:25:54 +0700

    feature/0005-refactor-migration

--- src/main/resources/db/migration/V01.00.04__insert_app_user.sql
+++ src/main/resources/db/migration/V01.00.04__insert_app_user.sql
@@ -1,7 +1,6 @@
 BEGIN TRANSACTION
 
-
-if not exists(select 1 from app_user where username = 'rootAdmin')
+    if not exists(select 1 from app_user where username = 'rootAdmin')
 insert app_user(created_by, last_modified_by, created_date, last_modified_date, email, is_active, password, username, app_role_id)
 values ('social_network_app_v1', 'social_network_app_v1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'rootAdmin@socialnetworkapp.com', 1,'$2a$10$y2Wsd4GRxdWPWBMfN8aOXOp8i3T4.0V2oEzMyfvQta62Ols8xjJkS','rootAdmin',1)
 
--- src/main/resources/db/migration/V1.01.01__insert_master_error_message.sql
+++
@@ -1,8 +0,0 @@
-BEGIN TRANSACTION
-    if not exists(select 1 from master_error_message where error_code = 'SEND_MAIL_ERROR')
-        insert master_error_message(created_by, last_modified_by, created_date, last_modified_date, error_code,
-                                    error_message)
-        values ('social_network_app_v1', 'social_network_app_v1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
-                'SEND_MAIL_ERROR',
-                'Error sending email to %s.')
-COMMIT TRANSACTION
